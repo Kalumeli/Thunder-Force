9f11a309fe311b04ae19d6b8989932e3b327d8ef
Angel-glory <angeirakoze71@gmail.com>
2025-02-20 23:38:08 -0800

    new function done by us lailah angel and roxy we the best

---
+++ function.js
@@ -0,0 +1,44 @@
+ //function call
+
+ function sayHi () {
+    console.log( "hello Angellailah and roxy");
+ }
+ sayHi();
+
+ //function apply
+
+ function sumUp (a,b){
+   
+    console.log(a+b);
+ }
+  sumUp.apply ( null,[10,12]);
+
+// function definition
+function callYourEx( greeting,name) {
+    console.log(greeting +  name);
+
+}
+callYourEx('hello','Angellailah');
+
+//bult-in function
+
+  function pickUp(){
+  let b= new Date();
+  console.log(b);
+  }
+    pickUp();
+
+ //bult in function
+function pickMe(){
+    let a= Math.random(10,50);
+    console.log(a);
+}
+pickMe();
+
+//biult in function
+
+function  meNow(){
+    let c= Date.now();
+    console.log(c);
+}
+meNow();
